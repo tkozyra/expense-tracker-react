8801843f4515141f84c07de79171ff511eec5503
tkozyra <tomekkozyra@gmail.com>
2021-04-09 09:15:03 +0200

    Update api url

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:8080/api";
+// export const API_URL = "http://localhost:8080/api";
+export const API_URL = "http://expense-tracker-api-java.herokuapp.com/api";
